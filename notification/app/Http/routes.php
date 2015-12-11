fade7a5757d8b53e49286a66a5c562a0b86b8adc
Deepak <deepakkumar222@gmail.com>
2015-12-12 04:25:15 +0530

    mongo enabled

--- notification/app/Http/routes.php
+++ notification/app/Http/routes.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('subscribe', function () {
-    return response()->json(['name' => 'Abigail', 'state' => 'CA']);
-});
+Route::resource('subscribe', 'Home');
