1386966b47c6a133292526da5f373319f0fdce66
Deepak <deepakkumar222@gmail.com>
2015-12-31 11:22:29 +0530

    adding redis

--- notification/resources/views/layouts/master.blade.php
+++ notification/resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PushFlix - The Holy Grail for Notifications</title>
+    <title>PushFlix</title>
 
     @if ( !Config::get('app.debug') )
         <link rel="stylesheet" href="/css/final.css" />
